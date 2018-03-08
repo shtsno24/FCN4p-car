07688d6d401c88309ada958b475cd6b8291a3a0c
shtsno24 <shtsno.24@gmail.com>
2018-03-08 21:44:05 +0900

    update

--- PC/net.py
+++ PC/net.py
@@ -7,16 +7,13 @@ import chainer.links as L
 class MLP(chainer.Chain):
 
     def __init__(self):
-        super(MLP, self).__init__(conv1=L.Convolution2D(1, 4, (3,8), stride=(3,8)),
+        super(MLP, self).__init__(conv1=L.DepthwiseConvolution2D(1, 4, (3,8), stride=(3,8)),
             conv2=L.Convolution2D(None, 4, 4, stride=2),
             conv3=L.Convolution2D(None, 16, 3, stride=2),
 
             deconv3 = L.Deconvolution2D(None,8,5,stride=5),
-            #deconv2 = L.Deconvolution2D(None,4,4,stride=2),
-            #deconv2 = L.Deconvolution2D(None,1,(3,1),stride=(3,1)),
             deconv1 = L.Deconvolution2D(None,1,(3,8),stride=(3,8)))
-            #deconv1 = L.Deconvolution2D(None,1,(1,8),stride=(1,8)))
-           
+                       
 
     def __call__(self, x):
         h = F.relu(self.conv1(x))
@@ -24,7 +21,5 @@ class MLP(chainer.Chain):
         h = F.relu(self.conv3(h))
 
         h = F.relu(self.deconv3(h))
-        #h = F.relu(self.deconv2(h))
         h = F.relu(self.deconv1(h))
-        
         return h
