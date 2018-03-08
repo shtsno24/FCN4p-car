1a3f4b14d0d72d56526de8d70d07e771e5d0db94
shtsno24 <shtsno.24@gmail.com>
2018-03-08 22:16:43 +0900

    update

--- PC/net.py
+++ PC/net.py
@@ -9,10 +9,11 @@ class MLP(chainer.Chain):
     def __init__(self):
         super(MLP, self).__init__(conv1=L.DepthwiseConvolution2D(1, 4, (3,8), stride=(3,8)),
             conv2=L.Convolution2D(None, 4, 4, stride=2),
-            conv3=L.Convolution2D(None, 16, 3, stride=2),
+            conv3=L.Convolution2D(None, 16,(2,2), stride=1),
 
-            deconv3 = L.Deconvolution2D(None,8,5,stride=5),
+            deconv3 = L.Deconvolution2D(None,8,6,stride=2),
             deconv1 = L.Deconvolution2D(None,1,(3,8),stride=(3,8)))
+            #deconv1 = L.Deconvolution2D(None,1,(15,40),stride=(15,40)))
                        
 
     def __call__(self, x):
